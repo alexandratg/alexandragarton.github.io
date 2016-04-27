f882bded03b30cf4e2cf946f65cf3a6cbb952bec
Alexandra Garton <axg1233@miami.edu>
2016-04-27 11:54:53 -0400

    arrows
    made false as they do not work

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 $('.single-item').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true,
+  arrows: false,
   fade: true,
   asNavFor: '.slider-nav',
   dots: true
 });
-// $('.single-item').slick({
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   asNavFor: '.slider-for',
-//   dots: true,
-//   centerMode: true,
-//   focusOnSelect: true
-// });
