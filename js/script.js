2c071e8823108756a9a1d0a2e4536d951b1e4507
Alexandra Garton <axg1233@miami.edu>
2016-04-27 10:54:48 -0400

    Added files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.single-item').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slider-nav',
+  dots: true
+});
+// $('.single-item').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: '.slider-for',
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true
+// });
